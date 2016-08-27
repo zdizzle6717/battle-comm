711b90a3b25a41a0112116e96add30937cba889a
Zack Anselm <zanselm5@gmail.com>
2016-08-27 14:51:06 +0000

    Fixed untracked files

--- RPVault/Connections/_environment.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once('braintree-php/lib/Braintree.php');
-    Braintree_Configuration::environment('sandbox');
-    Braintree_Configuration::merchantId('44b6x5hpcmvvqpyv');
-    Braintree_Configuration::publicKey('dyd22y3qy89v57b5');
-    Braintree_Configuration::privateKey('7e400db39161feb7b73c55d426a0a2c5');
-?>
