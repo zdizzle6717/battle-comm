b325ea3e5ff09e54f7112f18096556ec5ae96799
Zack Anselm <zanselm5@gmail.com>
2016-08-28 02:08:57 +0000

    Updated order success message

--- RPStore/App-Store/modules/main/views/orderSuccess.php
+++ RPStore/App-Store/modules/main/views/orderSuccess.php
@@ -2,7 +2,7 @@
 
     <h3>You order was successfully processed. You will receive a confirmation e-mail and a secondary e-mail when the products have shipped.</h3>
 
-    <h4>Thank you! We appreciate your business!</h4>
+    <h4>Thank you! We appreciate your participation in Battle-Comm!</h4>
 
 <div class="product-col-12" style="text-align:center;">
     <hr/>
