3ad9ad85ecfc3d399fc6f91b7bcedc47a7c11784
Zack Anselm <zanselm5@gmail.com>
2016-09-01 21:10:44 +0000

    Added store link to mobile nav

--- Templates/includes/top-navigation-mobile.php
+++ Templates/includes/top-navigation-mobile.php
@@ -5,6 +5,6 @@
         <li class="news"><a href="/News" ><h2 id="phoneOnly">NEWS</h2></a></li>
         <li class="events"><a href="/events.php" ><h2 id="phoneOnly">EVENTS</h2></a></li>
         <li class="logintab"><a href="/loginA.php" class="scrollDown"><h2 id="phoneOnly">LOGIN</h2></a></li>
-        <li class="registertab"><a href="/registrationA.php" class="scrollDown"><h2 id="phoneOnly">REGISTER</h2></a></li>
+        <li class="store"><a href="/RPStore/" class="scrollDown"><h2 id="phoneOnly">RP STORE</h2></a></li>
     </ul>
 </nav>
