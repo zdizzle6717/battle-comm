fa607e1e72f9d7d6a48580d233ac22149022938f
Zack Anselm <zanselm5@gmail.com>
2016-11-29 00:26:02 +0000

    Added a 404 page

---
+++ 404.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+	<title>Battle-Comm | Events</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<meta name="description" content="Find access to a worldwide community of dedicated table-top gamers and hobbyists as well as tools to promote your store, events, and gaming space to a worldwide community of dedicated table-top players. Earn system packs and a reward point vault for your future customers.">
+	<meta name="keyword" content="battle-comm, table-top, table, games, battle, event, warhammer 40k, worldwide, community, hobbyist, player, reward point, gaming, point, battlecomm, match, friendly, tournament, event, magic the gathering, star wars, warhammer">
+	<meta property="og:title" content="Battle-Comm | 404"/>
+	<meta property="og:url" content="https://www.battle-comm.net/events.php"/>
+	<meta property="og:image" content="https://www.battle-comm.net/images/meta-image.jpg"/>
+	<meta property="og:site_name" content="Battle-Comm"/>
+	<meta property="og:description" content="Find access to a worldwide community of dedicated table-top gamers and hobbyists as well as tools to promote your store, events, and gaming space to a worldwide community of dedicated table-top players. Earn system packs and a reward point vault for your future customers."/>
+
+	<link rel="stylesheet" type="text/css" media="screen, print" href="styles/global.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script type="text/javascript" src="Scripts/jquery.magnificant-popup.js"></script>
+</head>
+<?php include 'Templates/parts/header.php'; ?>
+	<?php include 'Templates/parts/container-top.php'; ?>
+		<div class="full_width text-center">
+            <h4><strong>404</strong>: Page not found</h4>
+		</div>
+	<?php include 'Templates/parts/container-bottom.php'; ?>
+<?php include 'Templates/parts/footer.php'; ?>
