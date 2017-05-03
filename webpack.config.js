50b62e04197b7e6aa60dec6604b043d3da418d71
Zachary Anselm <zachary.anselm@egov.com>
2017-05-03 11:14:37 -0400

    Added webpack compile config for client server

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ switch (process.env.NODE_ENV) {
     module.exports = require('./webpack.prod')({env: 'production'});
     break;
   case 'development':
-		module.exports = require('./webpack.dev')({env: 'development'});
+	module.exports = require('./webpack.dev')({env: 'development'});
+		break;
+  case 'client':
+	module.exports = require('./webpack.client')({env: 'production'});
 		break;
   default:
     module.exports = require('./webpack.dev')({env: 'development'});
